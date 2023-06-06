1efd3d98d19b76f25e2f5a64a8ec9fc2b81c75c6
rodyrahi <45394091+rodyrahi@users.noreply.github.com>
2023-06-06 21:24:16 +0530

    ok try

--- bundeli.js
+++ bundeli.js
@@ -1,10 +1,10 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 const axios = require('axios');
-var con = require("./database.js");
+var con = require('./database.js');
 const qrcode = require('qrcode-terminal');
+const { Client, LocalAuth } = require('whatsapp-web.js');
 
-const { Client , LocalAuth } = require('whatsapp-web.js');
 const client = new Client({
   restartOnAuthFail: true,
   puppeteer: {
@@ -16,37 +16,30 @@ const client = new Client({
       "--disable-accelerated-2d-canvas",
       "--no-first-run",
       "--no-zygote",
-      "--single-process", // <- this one doesn't works in Windows
+      "--single-process", // <- this one doesn't work in Windows
       "--disable-gpu",
       "--use-gl=egl",
     ],
   },
-  authStrategy: new LocalAuth(),
-  
+  authStrategy: new LocalAuth({
+    clientId: 'raj',
+  }),
 });
 
 client.on('qr', qr => {
-    qrcode.generate(qr, {small: true});
+  qrcode.generate(qr, { small: true });
 });
 
 client.on('ready', () => {
   console.log('Client is ready!');
-
 });
 
 client.initialize();
 
+app.set('view engine', 'ejs');
 
-
-
-
-
-app.set('view engine', 'ejs')
-
-app.use(express.static("public"));
-app.use(express.urlencoded({extended:false}))
-
-
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: false }));
 
 function executeQuery(query) {
   return new Promise((resolve, reject) => {
@@ -60,99 +53,94 @@ function executeQuery(query) {
   });
 }
 
-
 app.get('/', (req, res) => {
+  res.render('loginpage');
+});
 
+app.get('/createprofile/:number', async (req, res) => {
+  const number = req.params.number;
+  res.render('createprofile', { phonenumber: number });
+});
 
-  res.render('loginpage')
-  
-  
-  })
+app.post('/createprofile/:number', async (req, res) => {
+  const number = req.params.number;
+  const { name, fathername, gender, dob, pincode, address } = req.body;
 
+  await executeQuery(
+    `INSERT INTO kissans (number,name , fathername , gender , dob , pincode , address) VALUES ('${number}','${name}','${fathername}' ,'${gender}' ,'${dob}','${pincode}','${address}')`
+  );
 
+  res.redirect('/home/' + number);
+});
 
-app.get('/createprofile/:number',async (req, res) => {
-
-  const number = req.params.number
-  res.render('createprofile' , {phonenumber : number})
-
-})
-app.post('/createprofile/:number',async (req, res) => {
-
-  const number = req.params.number
-  const {name , fathername , gender , dob , pincode , address} = req.body
-
-  await executeQuery(`INSERT INTO kissans (number,name , fathername , gender , dob , pincode , address) VALUES ('${number}','${name}','${fathername}' ,'${gender}' ,'${dob}','${pincode}','${address}')`)
-
-  res.redirect('/home/'+number)
-
-})
-
-app.get('/userprofile/:number',async (req, res) => {
-
-  const number = req.params.number
-  const result = await executeQuery(`SELECT * FROM kissans WHERE number='${number}' `) 
+app.get('/userprofile/:number', async (req, res) => {
+  const number = req.params.number;
+  const result = await executeQuery(
+    `SELECT * FROM kissans WHERE number='${number}'`
+  );
 
   console.log(result);
 
-  res.render('userprofile' , {phonenumber : number , user:result[0]})
+  res.render('userprofile', { phonenumber: number, user: result[0] });
+});
 
-})
-
-app.get('/home/:phonenumber', (req, res) => {
+app.get('/home/:phonenumber', async (req, res) => {
   const phonenumber = req.params.phonenumber;
   console.log(phonenumber);
 
-  axios(`https://api.openweathermap.org/data/2.5/weather?id=1264542&appid=404ae0fc6125b1b2ac81edc980993a31`)
-    .then(response => {
-      res.render('home', { weather: response.data, phonenumber:phonenumber });
-    })
-    .catch(error => {
-      console.log('Error:', error);
-    });
+  try {
+    const response = await axios.get(
+      'https://api.openweathermap.org/data/2.5/weather?id=1264542&appid=404ae0fc6125b1b2ac81edc980993a31'
+    );
 
-  // Issue: Multiple response can be sent if an error occurs or if the request is successful.
-
+    res.render('home', { weather: response.data, phonenumber: phonenumber });
+  } catch (error) {
+    console.log('Error:', error);
+    res.sendStatus(500); // Send an error response to the client
+  }
 });
 
-app.get('/chat/:number',async (req, res) => {
-  const number = req.params.number
+app.get('/chat/:number', async (req, res) => {
+  const number = req.params.number;
 
   console.log(number);
   try {
-    const result =  await executeQuery(`SELECT (chat) FROM chats WHERE number='${number}'`)
-    
+    const result = await executeQuery(
+      `SELECT (chat) FROM chats WHERE number='${number}'`
+    );
+
     console.log(result);
-    res.render('chat' , { phonenumber:number,chats:result})
-
+    res.render('chat', { phonenumber: number, chats: result });
   } catch (error) {
-    res.render('chat' , {phonenumber:number})
+    console.log(error);
+    res.render('chat', { phonenumber: number });
   }
-
-})
+});
 
 app.post('/savechat/:number', async (req, res) => {
-  
-  const number = req.params.number
+  const number = req.params.number;
 
   console.log(req.body);
-  
-  const { textInput  } = req.body
 
+  const { textInput } = req.body;
 
-  const name = await executeQuery(`SELECT (name) FROM kissans WHERE number='${number}'`)
-  await executeQuery(`INSERT INTO chats (chat , number ,name) VALUES ('${textInput}' , '${number}', '${name[0].name}')`)
-  
-  
-  res.redirect( '/chat/'+number)
-})
+  const name = await executeQuery(
+    `SELECT (name) FROM kissans WHERE number='${number}'`
+  );
 
-var randomCode
+  await executeQuery(
+    `INSERT INTO chats (chat , number ,name) VALUES ('${textInput}' , '${number}', '${name[0].name}')`
+  );
+
+  res.redirect('/chat/' + number);
+});
+
+var randomCode;
+
 app.post('/sendcode', async (req, res) => {
-  
-  const { phonenumber } = req.body
-  const number = '+91'+phonenumber
-  console.log(req.body);
+  const { phonenumber } = req.body;
+  const number = '+91' + phonenumber;
+
   const generateRandomCode = () => {
     const codeLength = 6;
     let code = '';
@@ -161,55 +149,55 @@ app.post('/sendcode', async (req, res) => {
     }
     return code;
   };
-  
+
   randomCode = generateRandomCode();
 
   console.log(number);
-  client.sendMessage(`${+number}@c.us`, randomCode)
-  .then(() => {
-    console.log('Message sent successfully');
+  client
+    .sendMessage(`${+number}@c.us`, randomCode)
+    .then(() => {
+      console.log('Message sent successfully');
+      const result = executeQuery(`SELECT * FROM chats WHERE number='${number}'`);
 
-  })
-  .catch((error) => {
-    console.error('Error sending message:', error);
+      if (result.length < 1) {
+        executeQuery(`INSERT INTO chats (number) VALUES ('${number}')`);
+      }
 
-  });
-
-
-  const result = await executeQuery(`SELECT * FROM chats WHERE number='${number}'`)
-
-  if (result.length < 1) {
-    await executeQuery(`INSERT INTO chats (number) VALUES ('${number}')`)
-
-  }
-
-
-  res.render('typecode' , {number:number})
-})
+      res.render('typecode', { number: number });
+    })
+    .catch((error) => {
+      console.error('Error sending message:', error);
+      res.sendStatus(500); // Send an error response to the client
+    });
+});
 
 app.post('/numberlogin', async (req, res) => {
-  const {code , phonenumber} = req.body
+  const { code, phonenumber } = req.body;
+
+  const user = await executeQuery(
+    `SELECT * FROM kissans WHERE number='${phonenumber}'`
+  );
 
   if (code === randomCode) {
-    res.redirect('/createprofile/'+phonenumber )
-
+    if (user.length > 0) {
+      res.redirect('/home/' + phonenumber);
+    } else {
+      res.redirect('/createprofile/' + phonenumber);
+    }
+  } else {
+    res.render('whatsapplogin');
   }
-  res.render('whatsapplogin')
+});
 
-})
+app.get('/whatsapplogin', async (req, res) => {
+  res.render('whatsapplogin');
+});
 
-app.get('/whatsapplogin',async (req, res) => {
-    res.render('whatsapplogin')
-})
+app.get('/query', async (req, res) => {
+  const chats = await executeQuery('SELECT * FROM chats');
+  res.render('query', { chats: chats });
+});
 
-app.get('/query',async (req, res) => {
-
-  const chats = await executeQuery(`SELECT * FROM chats`)
-  res.render('query' , {chats:chats})
-  
-  
-  })
-
-
-
-app.listen(7777)
+app.listen(7777, () => {
+  console.log('Server is running on port 7777');
+});
