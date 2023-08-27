25b00f6dae6b27e5bf2101979ece5634b4ff2e03
rodyrahi <45394091+rodyrahi@users.noreply.github.com>
2023-08-27 13:12:03 +0530

    logout fixed

--- bundeli.js
+++ bundeli.js
@@ -371,15 +371,18 @@ app.get('/mandi', async (req, res) => {
 
 
 app.get('/logout', (req, res) => {
+  res.render('partials/logoutwarning')
+
+});
+
+
+app.get('/deletesession', (req, res) => {
   req.session.destroy(err => {
-      if (err) {
-          console.error('Error destroying session:', err);
-      } else {
-          console.log('Session destroyed');
-      }
-      res.render('partials/logoutwarning')
+
+      res.redirect('/')
   });
 });
+
 
 app.listen(7777, () => {
   console.log('Server is running on port 7777');
